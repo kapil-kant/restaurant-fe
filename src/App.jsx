fb32653a37da41b56261fca788a5da668e7adbc2
Github App V1 <github-app-v1@nexon.ai>
2026-08-24 11:20:49 +0000

    Implement solution for issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Restaurant FE</h1>
+      <h1 className="App-title">Welcome to Restaurant FE</h1>
       <p>This is the basic boilerplate setup.</p>
-    </div>
+    </div >
   );
 }
 
